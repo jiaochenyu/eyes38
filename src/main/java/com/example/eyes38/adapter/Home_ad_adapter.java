6ed8c411a79a97b7430bdd0fdf905cdcd95090cc
huangjiechun <442886230@qq.com>
2016-05-20 20:40:04 +0800

    5月20日,更换了app的图标,制作了首页的上部分内容

---
+++ src/main/java/com/example/eyes38/adapter/Home_ad_adapter.java
@@ -0,0 +1,55 @@
+package com.example.eyes38.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.eyes38.fragment.HomeFragment;
+
+import java.util.List;
+
+/**
+ * Created by huangjiechun on 16/5/19.
+ */
+public class Home_ad_adapter  extends PagerAdapter {
+    List<View> mViewList;
+
+    public Home_ad_adapter(List<View> viewList, HomeFragment mainActivity) {
+        mViewList = viewList;
+    }
+
+    @Override
+    public int getCount() {
+        //设定为最大值,确保能一直轮播
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        //viewGroup是装在图片布局的容器
+        position %= mViewList.size();
+        if (position<0){
+            position += mViewList.size();
+        }
+        View view = mViewList.get(position);
+        ViewParent parent = view.getParent();
+        if (parent!=null){
+            ViewGroup vg = (ViewGroup) parent;
+            vg.removeView(view);
+        }
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+//        super.destroyItem(container, position, object);
+    }
+}
--- src/main/java/com/example/eyes38/fragment/HomeFragment.java
+++ src/main/java/com/example/eyes38/fragment/HomeFragment.java
@@ -2,14 +2,21 @@ package com.example.eyes38.fragment;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.eyes38.MainActivity;
 import com.example.eyes38.R;
+import com.example.eyes38.adapter.Home_ad_adapter;
+
+import java.util.ArrayList;
 
 
 /**
@@ -18,11 +25,91 @@ import com.example.eyes38.R;
 public class HomeFragment extends Fragment {
     MainActivity mMainActivity;
     View view;
+    public static final int IMAGE_UPDATE = 1;
+    public static final int REFRESHTIME = 5 * 1000;
+    public static final int IMAGE_CHANGED = 2;
+    ViewPager mViewPager;
+    ArrayList<View> mViewList;
+    int mCurrentItem = Integer.MAX_VALUE / 2;
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.home, null);
         mMainActivity = (MainActivity) getActivity();
-        view = inflater.inflate(R.layout.home,null);
+        initView();
+        initData(savedInstanceState);
+        setLinstener();
+        Home_ad_adapter myAdapter = new Home_ad_adapter(mViewList,this);
+        mViewPager.setAdapter(myAdapter);
+        mViewPager.setCurrentItem(mCurrentItem);
+        mHandler.sendEmptyMessageDelayed(IMAGE_UPDATE,REFRESHTIME);
         return view;
     }
+
+    private void setLinstener() {
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
+                //手动滑到这个广告的时候,发送改位置的值
+                mHandler.sendMessage(Message.obtain(mHandler,IMAGE_CHANGED,position,0));
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void initData(Bundle savedInstanceState) {
+        mViewList = new ArrayList<View>();
+        int[] imageId = {R.drawable.banner01, R.drawable.banner1,R.drawable.banner01,R.drawable.banner1};
+        for (int i = 0; i < imageId.length; i++) {
+            View view = getLayoutInflater(savedInstanceState).inflate(R.layout.home_ad_item, null);
+            ImageView mItemIvContent = (ImageView) view.findViewById(R.id.item_iv_content);
+            mItemIvContent.setImageResource(imageId[i]);
+            mViewList.add(view);
+        }
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) view.findViewById(R.id.main_ad_show);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+    Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            int action = msg.what;
+            if (mHandler.hasMessages(IMAGE_UPDATE)){
+                mHandler.removeMessages(IMAGE_UPDATE);
+            }
+            switch (action){
+                case IMAGE_UPDATE:
+                    //轮播图经行更新
+                    mCurrentItem +=1;
+                    mViewPager.setCurrentItem(mCurrentItem);
+                    mHandler.sendEmptyMessageDelayed(IMAGE_UPDATE, REFRESHTIME);
+                    break;
+                case IMAGE_CHANGED:
+                    //手动滑了广告
+                    mCurrentItem = msg.arg1;
+                    mViewPager.setCurrentItem(mCurrentItem);
+                    mHandler.sendEmptyMessageDelayed(IMAGE_UPDATE,REFRESHTIME);
+                    break;
+            }
+        }
+    };
+
 }
