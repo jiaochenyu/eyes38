1ff7c9aa514aaefc4caee8f670fdbe2383419a53
weixiao <734474820@qq.com>
2016-05-25 08:40:54 +0800

    cfj "新建一周"

--- src/main/java/com/example/eyes38/adapter/EventRecycleViewAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.example.eyes38.adapter;
-
-import android.support.v7.widget.RecyclerView;
-
-/**
- * Created by weixiao on 2016/5/24.
- */
-public class EventRecycleViewAdapter extends RecyclerView.Adapter<EventRecycleViewAdapter.EventRecycleViewHolder>{
-}
