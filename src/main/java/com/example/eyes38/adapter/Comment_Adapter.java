b468d51b18746807f9896411882e9b3f022f19ce
Chenziquan <Chenjunquan0208@gmail.com>
2016-05-28 09:07:43 +0800

    陈军权5.28

--- src/main/java/com/example/eyes38/adapter/Comment_Adapter.java
+++ src/main/java/com/example/eyes38/adapter/Comment_Adapter.java
@@ -1,5 +1,7 @@
 package com.example.eyes38.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +11,10 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.eyes38.R;
+import com.example.eyes38.activity.CommentReplyActivity;
 import com.example.eyes38.beans.Comments;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -18,10 +22,13 @@ import java.util.List;
  */
 public class Comment_Adapter extends RecyclerView.Adapter<Comment_Adapter.MyViewHolder> {
     List<Comments> mList;
-    public Comment_Adapter(List<Comments> mList) {
-        this.mList = mList;
-    }
+    Context context;
 
+
+    public Comment_Adapter(List<Comments> mList, Context context) {
+        this.mList = mList;
+        this.context = context;
+    }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -33,11 +40,19 @@ public class Comment_Adapter extends RecyclerView.Adapter<Comment_Adapter.MyView
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
         holder.comment_usernamTextView.setText(mList.get(position).getName());
         holder.comment_contentTextView.setText(mList.get(position).getContent());
         holder.comment_timeTextView.setText(mList.get(position).getTime());
         holder.mRatingBar.setRating(mList.get(position).getRatingbar());
+        holder.replyImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, CommentReplyActivity.class);
+                intent.putExtra("values", (Serializable) mList.get(position).getReplyList());
+                context.startActivity(intent);
+            }
+        });
 
     }
 
@@ -47,7 +62,7 @@ public class Comment_Adapter extends RecyclerView.Adapter<Comment_Adapter.MyView
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
-        ImageView portraitImageView;
+        ImageView portraitImageView,replyImageView;
         RatingBar mRatingBar;
         TextView comment_usernamTextView, comment_contentTextView, comment_timeTextView;
 
@@ -57,6 +72,7 @@ public class Comment_Adapter extends RecyclerView.Adapter<Comment_Adapter.MyView
             comment_usernamTextView = (TextView) itemView.findViewById(R.id.comment_username);
             comment_contentTextView = (TextView) itemView.findViewById(R.id.comment_content);
             comment_timeTextView = (TextView) itemView.findViewById(R.id.comment_time);
+            replyImageView = (ImageView) itemView.findViewById(R.id.comment_reply);
         }
     }
 
